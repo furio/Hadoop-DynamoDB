5873e0fea6ca0ab816909008e3f27b97f705b6ed
Neal Clark <neal@willetinc.com>
2012-11-22 12:08:39 -0500

    Added license to source files

--- src/main/java/com/willetinc/hadoop/mapreduce/dynamodb/AbstractSplitter.java
+++ src/main/java/com/willetinc/hadoop/mapreduce/dynamodb/AbstractSplitter.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2012 Willet Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.willetinc.hadoop.mapreduce.dynamodb;
 
 import java.io.IOException;
@@ -29,23 +45,16 @@ public abstract class AbstractSplitter implements DynamoDBSplitter {
 	public List<InputSplit> split(Configuration conf) throws IOException {
 
 		// load configuration
-		boolean interpolate =
-				DynamoDBQueryInputFormat
-						.getInterpolateAcrossRangeKeyValues(conf);
+		boolean interpolate = DynamoDBQueryInputFormat.getInterpolateAcrossRangeKeyValues(conf);
 
 		Types hashKeyType = DynamoDBQueryInputFormat.getHashKeyType(conf);
-		AttributeValue hashKeyValue =
-				DynamoDBQueryInputFormat.getHashKeyValue(conf);
+		AttributeValue hashKeyValue = DynamoDBQueryInputFormat.getHashKeyValue(conf);
 
 		Types rangeKeyType = DynamoDBQueryInputFormat.getRangeKeyType(conf);
-		Collection<AttributeValue> rangeKeyValues =
-				DynamoDBQueryInputFormat.getRangeKeyValues(conf);
-		ComparisonOperator rangeKeyoperator =
-				DynamoDBQueryInputFormat.getRangeKeyComparisonOperator(conf);
-		AttributeValue minRangeKeyValue =
-				DynamoDBQueryInputFormat.getRangeKeyInterpolateMinValue(conf);
-		AttributeValue maxRangeKeyValue =
-				DynamoDBQueryInputFormat.getRangeKeyInterpolateMaxValue(conf);
+		Collection<AttributeValue> rangeKeyValues = DynamoDBQueryInputFormat.getRangeKeyValues(conf);
+		ComparisonOperator rangeKeyoperator = DynamoDBQueryInputFormat.getRangeKeyComparisonOperator(conf);
+		AttributeValue minRangeKeyValue = DynamoDBQueryInputFormat.getRangeKeyInterpolateMinValue(conf);
+		AttributeValue maxRangeKeyValue = DynamoDBQueryInputFormat.getRangeKeyInterpolateMaxValue(conf);
 
 		// ensure DynamoDBQueryInputFormat was configured correctly
 		if (interpolate) {
@@ -66,17 +75,18 @@ public abstract class AbstractSplitter implements DynamoDBSplitter {
 		List<InputSplit> splits = new ArrayList<InputSplit>();
 
 		// handle cases where interpolation is turned off or unnecessary
-		if (!interpolate || numRangeSplits <= 1 || minRangeKeyValue == null
+		if (!interpolate
+				|| numRangeSplits <= 1
+				|| minRangeKeyValue == null
 				|| maxRangeKeyValue == null) {
 			LOG.info("Generating 1 split for each HashKey");
 
-			DynamoDBQueryInputFormat.DynamoDBQueryInputSplit split =
-					new DynamoDBQueryInputFormat.DynamoDBQueryInputSplit(
-							hashKeyType,
-							hashKeyValue,
-							rangeKeyType,
-							rangeKeyValues,
-							rangeKeyoperator);
+			DynamoDBQueryInputFormat.DynamoDBQueryInputSplit split = new DynamoDBQueryInputFormat.DynamoDBQueryInputSplit(
+					hashKeyType,
+					hashKeyValue,
+					rangeKeyType,
+					rangeKeyValues,
+					rangeKeyoperator);
 
 			splits.add(split);
 		} else {
